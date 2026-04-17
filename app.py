d5876ffe9d5d0d8725281cf8121c3031660f9896
TIRUVEEDHI VENKATA PAVAN KUMAR <pavanthiriveedi7@gmail.com>
2026-04-17 21:31:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import joblib
 import json
+import sklearn.ensemble import RandomForestClassifier
 import plotly.graph_objects as go
 import plotly.express as px
 from plotly.subplots import make_subplots
